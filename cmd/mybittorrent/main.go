6994252e8ee8811e38bb9daa9ccebc082795ea89
nikhilk <nikhilk@arista.com>
2024-05-21 05:01:39 +0530

    test

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -8,71 +8,43 @@ import (
 	"unicode"
 )
 
-func decodeBencode(bencodedString string, start int, end int) ([]interface{}, bool) {
-	if start > end {
-		return []interface{}{}, false
-	}
-	if bencodedString[start] == 'l' {
-		result, wrapper := decodeBencode(bencodedString, start+1, end-1)
+// Stack represents a stack that holds a slice of empty interfaces (to allow for different types)
+type Stack struct {
+	elements []interface{}
+}
 
-		returnResult := []interface{}{}
-		if wrapper {
-			returnResult = append(returnResult, result)
-		} else {
-			i := 0
-			for i < len(result) {
-				returnResult = append(returnResult, result[i])
-				i++
-			}
-		}
-		return returnResult, true
-	}
-	if unicode.IsDigit(rune(bencodedString[start])) {
-		var firstColonIndex int
-		for i := start; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-		lengthStr := bencodedString[start:firstColonIndex]
-		length, _ := strconv.Atoi(lengthStr)
+// Push adds an element to the top of the stack
+func (s *Stack) Push(element interface{}) {
+	s.elements = append(s.elements, element)
+}
 
-		result, wrapper := decodeBencode(bencodedString, firstColonIndex+1+length, end)
-		returnResult := []interface{}{bencodedString[firstColonIndex+1 : firstColonIndex+1+length]}
-		if wrapper {
-			returnResult = append(returnResult, result)
-		} else {
-			i := 0
-			for i < len(result) {
-				returnResult = append(returnResult, result[i])
-				i++
-			}
-		}
-		return returnResult, false
-	} else if bencodedString[start] == 'i' {
-		lastIndex := 0
-		for i := start + 1; i < len(bencodedString); i++ {
-			if bencodedString[i] == 'e' {
-				lastIndex = i
-				break
-			}
-		}
-		num, _ := strconv.Atoi(bencodedString[start+1 : lastIndex])
-		result, wrapper := decodeBencode(bencodedString, lastIndex+1, end)
-		returnResult := []interface{}{num}
-		if wrapper {
-			returnResult = append(returnResult, result)
-		} else {
-			i := 0
-			for i < len(result) {
-				returnResult = append(returnResult, result[i])
-				i++
-			}
-		}
-		return returnResult, false
-	} else {
-		return nil, true
+// Pop removes and returns the top element of the stack. Returns an error if the stack is empty.
+func (s *Stack) Pop() interface{} {
+	topElement := s.elements[len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
+	return topElement
+}
+
+// Peek returns the top element of the stack without removing it. Returns an error if the stack is empty.
+func (s *Stack) Peek() interface{} {
+	return s.elements[len(s.elements)-1]
+}
+
+// IsEmpty checks if the stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(s.elements) == 0
+}
+
+// Size returns the number of elements in the stack
+func (s *Stack) Size() int {
+	return len(s.elements)
+}
+
+func reverse(slice *[]interface{}) {
+	length := len(*slice)
+	for i := 0; i < length/2; i++ {
+		j := length - i - 1
+		(*slice)[i], (*slice)[j] = (*slice)[j], (*slice)[i]
 	}
 }
 
@@ -82,11 +54,54 @@ func main() {
 
 	if command == "decode" {
 		bencodedValue := os.Args[2]
-		decoded, wrapper := decodeBencode(bencodedValue, 0, len(bencodedValue)-1)
-		jsonOutput, _ := json.Marshal(decoded)
-		if !wrapper {
-			jsonOutput, _ = json.Marshal(decoded[0])
+		stack := &Stack{}
+		i := 0
+		for i < len(bencodedValue) {
+			if bencodedValue[i] == 'l' {
+				stack.Push('l')
+				i = i + 1
+			} else if bencodedValue[i] == 'e' {
+				list := []interface{}{}
+				for {
+					if stack.Peek() != 'l' {
+						list = append(list, stack.Peek())
+						stack.Pop()
+					} else {
+						stack.Pop()
+						reverse(&list)
+						stack.Push(list)
+						break
+					}
+				}
+				i = i + 1
+			} else if unicode.IsDigit(rune(bencodedValue[i])) {
+				var firstColonIndex int
+				for j := i; j < len(bencodedValue); j++ {
+					if bencodedValue[j] == ':' {
+						firstColonIndex = j
+						break
+					}
+				}
+				lengthStr := bencodedValue[i:firstColonIndex]
+				length, _ := strconv.Atoi(lengthStr)
+
+				letter := bencodedValue[firstColonIndex+1 : firstColonIndex+1+length]
+				stack.Push(letter)
+				i = firstColonIndex + 1 + length
+			} else if bencodedValue[i] == 'i' {
+				lastIndex := 0
+				for j := i + 1; j < len(bencodedValue); j++ {
+					if bencodedValue[j] == 'e' {
+						lastIndex = j
+						break
+					}
+				}
+				num, _ := strconv.Atoi(bencodedValue[i+1 : lastIndex])
+				stack.Push(num)
+				i = lastIndex + 1
+			}
 		}
+		jsonOutput, _ := json.Marshal(stack.Peek())
 		fmt.Println(string(jsonOutput))
 	} else {
 		fmt.Println("Unknown command: " + command)
