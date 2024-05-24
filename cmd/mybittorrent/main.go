e234724bc4bbbe2e8073ac91f55f3284e12ad9e1
nikhilk <nikhilk@arista.com>
2024-05-24 22:28:19 +0530

    test

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -282,7 +282,7 @@ func getPieceBytes(conn net.Conn, pieceID int) []byte {
 		for len(allcombined) < total {
 			tempBuffer := make([]byte, total-len(allcombined))
 			bytesRead, _ := conn.Read(tempBuffer)
-			// fmt.Println(pieceID, i, bytesRead)
+			fmt.Println(pieceID, i, bytesRead)
 			allcombined = append(allcombined, tempBuffer[:bytesRead]...)
 		}
 		fmt.Println(allcombined[:13])
