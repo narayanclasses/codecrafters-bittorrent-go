e458a85523850e25ac3a9e3f6808fc3e6c423ac7
nikhilk <nikhilk@arista.com>
2024-05-24 11:48:10 +0530

    test

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -260,7 +260,7 @@ func getPieceBytes(conn net.Conn, pieceID int) []byte {
 		// read response
 		response := make([]byte, 4+1+4+4+int(math.Pow(2, 14)))
 		conn.Read(response)
-		pieceBytes = append(pieceBytes, response[13:13+pieceLength]...)
+		pieceBytes = append(pieceBytes, response[13:13+curPieceLen]...)
 	}
 	return pieceBytes
 }
