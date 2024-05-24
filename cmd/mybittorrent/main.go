1a77d5377362ef6ab7e63e3ea6ba4c0a3c8b7c72
nikhilk <nikhilk@arista.com>
2024-05-24 07:43:48 +0530

    test

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -244,7 +244,7 @@ func main() {
 		conn, _ := net.Dial("tcp", peersArray[0])
 		defer conn.Close()
 		conn.Write(getHandShakeMessage())
-		buffer := make([]byte, 128)
+		buffer := make([]byte, 512)
 		conn.Read(buffer)
 		fmt.Println(buffer)
 		// }
